6184ae565b565880238d04646436be6cd1248baa
Debojyoti Singha <debojyoti@aeologic.com>
2022-02-15 07:30:27 +0530

    color-added

--- app/src/main/java/com/swing/soulscape/features/splash/FlashActivity.kt
+++ app/src/main/java/com/swing/soulscape/features/splash/FlashActivity.kt
@@ -1,11 +1,12 @@
-package com.swing.soulscape
+package com.swing.soulscape.features.splash
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.swing.soulscape.R
 
-class MainActivity : AppCompatActivity() {
+class FlashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_flash)
     }
 }
