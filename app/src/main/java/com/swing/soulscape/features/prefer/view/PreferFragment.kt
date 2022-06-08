e949090bcab358d7c4ad9aaee6d30e245672cd87
Debojyoti Singha <debojyoti@aeologic.com>
2022-06-08 19:12:27 +0530

    viewpager-added

--- app/src/main/java/com/swing/soulscape/features/prefer/view/PreferFragment.kt
+++ app/src/main/java/com/swing/soulscape/features/prefer/view/PreferFragment.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.viewpager2.widget.ViewPager2
 import com.swing.soulscape.adapter.PreferImageViewAdapter
+import com.swing.soulscape.adapter.PreferListAdapter
 import com.swing.soulscape.base.BaseFragment
 import com.swing.soulscape.databinding.FragmentPreferBinding
 import com.swing.soulscape.db.LocalDb
+import com.swing.soulscape.features.custom_view.DepthPageTransformer
 import com.swing.soulscape.utils.Extensions.logoRotate
 
 class PreferFragment : BaseFragment<FragmentPreferBinding>() {
@@ -17,18 +20,28 @@ class PreferFragment : BaseFragment<FragmentPreferBinding>() {
         imageList = LocalDb.generatePreferList()
     )
 
+    private val listAdapter = PreferListAdapter(
+        dataList = LocalDb.generatePreferList().toMutableList()
+            .subList(1, LocalDb.generatePreferList().size),
+        listener = {
+            binding.topImageViewPager.currentItem = it + 1
+        }
+    )
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.logo.flashImg.logoRotate()
-
         binding.topImageViewPager.adapter = adapter
-
+        binding.topImageViewPager.setPageTransformer(DepthPageTransformer())
         binding.topImageViewPager.registerOnPageChangeCallback(object :
             ViewPager2.OnPageChangeCallback() {
             override fun onPageSelected(position: Int) {
 
             }
         })
+
+        binding.itemListView.layoutManager = LinearLayoutManager(context)
+        binding.itemListView.adapter = listAdapter
     }
 
     override fun vmObserver() {
